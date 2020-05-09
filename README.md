090781d54f7630d69587b39decf305349fb298ec
Kyli Bing <kylidboy@outlook.com>
2020-05-09 14:48:16 +0800

    feat: configurable

---
+++ README.md
@@ -0,0 +1,11 @@
+## config
+
+ ``` toml
+[btc_price]
+interval=persist
+LABEL_BTC=<span font='Font Awesome 5 Free'>&#xf379;</span>
+LABEL_ETH=<span font='Font Awesome 5 Free'>&#xf42e;</span>
+LABEL_USD=<span font='Font Awesome 5 Free'>&#xf155;</span>
+ ```
+*LABELs are optional*
+ 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use serde::{Serialize, Deserialize};
 use std::time::Duration;
+use std::env;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct FeiXiaoHao {
@@ -9,6 +10,10 @@ struct FeiXiaoHao {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let label_btc = env::var("LABEL_BTC");
+    let label_eth = env::var("LABEL_ETH");
+    let label_usd = env::var("LABEL_USD");
+
     let headers = http::HeaderMap::new();
     let client = reqwest::Client::builder().timeout(Duration::from_secs(10)).default_headers(headers).build().unwrap();
     let mut feixiaohao_list: Vec<FeiXiaoHao> = vec![];
@@ -19,13 +24,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .json()
         .await?;
         for i in feixiaohao_list.iter() {
-            if i.id.eq("bitcoin") {
-                s.push(format!("<span font='Font Awesome 5 Free'>&#xf379;</span> {}<span font='Font Awesome 5 Free'>&#xf155;</span>", i.price_usd));
-            } else if i.id.eq("ethereum") {
-                s.push(format!("<span font='Font Awesome 5 Free'>&#xf42e;</span> {}<span font='Font Awesome 5 Free'>&#xf155;</span>", i.price_usd));
+            if i.id.eq("bitcoin") && label_btc.is_ok() {
+                let mut btc = format!("{} {}", label_btc.clone().unwrap(), i.price_usd);
+                if label_usd.is_ok() {
+                    btc = format!("{}{}", &btc, label_usd.clone().unwrap());
+                }
+                s.push(btc);
+            } else if i.id.eq("ethereum") && label_eth.is_ok() {
+                let mut eth = format!("{} {}", label_eth.clone().unwrap(), i.price_usd);
+                if label_usd.is_ok() {
+                    eth = format!("{}{}", &eth, label_usd.clone().unwrap());
+                }
+                s.push(eth);
             }
         }
-        println!("{}", s.join::<&str>(" "));
+        println!("{}", s.join::<&str>("   "));
         feixiaohao_list.truncate(0);
         s.truncate(0);
         std::thread::sleep(Duration::from_secs(30));
